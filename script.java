68842fe4eecba75a2a27a3209120c03b7db5df92
Matheus Silva <matheus1882014@outlook.com>
2025-09-21 16:32:40 -0300

    commit inicial

---
+++ script.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+
+
+public class Main {
+public static void main(String[] args) {
+   
+   
+   int xp = 0;
+String nome;
+
+Scanner scanner = new Scanner(System.in);
+
+
+System.out.println("Digite seu nome de usuario: ");
+   nome = scanner.nextLine();
+
+
+   System.out.println("Oi, prazer te conhecer " + nome);
+ System.out.println("O seu xp a seguir: " + xp);
+
+}
+
+
+
+}
+
