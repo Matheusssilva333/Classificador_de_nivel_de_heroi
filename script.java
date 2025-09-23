064b1341178326bd0eac1664b7105f52b2d63357
Matheus de Souza Silva <matheus1882014@outlook.com>
2025-09-23 16:54:41 -0300

    Update script.java

--- script.java
+++ script.java
@@ -1,28 +1,30 @@
-
 import java.util.Scanner;
 
+public class Heroi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite seu nome de usuário: ");
+        String nome = scanner.nextLine();
 
-public class Main {
-public static void main(String[] args) {
-   
-   
-   int xp = 0;
-String nome;
+        System.out.println("Digite seu XP: ");
+        int xp = scanner.nextInt();
 
-Scanner scanner = new Scanner(System.in);
+        System.out.println("Oi, prazer te conhecer " + nome);
+        System.out.println("O seu XP a seguir: " + xp);
 
+        // Usando if-else para intervalos de XP
+        if (xp <= 1000) {
+            System.out.println("Seu nível é igual a: Ferro");
+        } else if (xp <= 2000) {
+            System.out.println("Seu nível é igual a: Bronze");
+        } else if (xp <= 5000) {
+            System.out.println("Seu nível é igual a: Prata");
+        } else {
+            System.out.println("Seu nível é maior que Prata!");
+        }
 
-System.out.println("Digite seu nome de usuario: ");
-   nome = scanner.nextLine();
-
-
-   System.out.println("Oi, prazer te conhecer " + nome);
- System.out.println("O seu xp a seguir: " + xp);
-
-}
-
-
-
+        scanner.close();
+    }
 }
 
